91fcb98665f2c1b08aac8d3b2dbbbcdc2b56e923
Naya  Willis <greedybrain18@gmail.com>
2019-10-11 20:55:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,11 @@ class Song
     artist.songs << self
   end
   
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end
+  
   def self.all 
     @@all 
   end
